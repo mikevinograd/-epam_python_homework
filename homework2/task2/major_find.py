65bfef060acfeb1d840d3755413b86b4b1c62763
mikevinograd <33091915+mikevinograd@users.noreply.github.com>
2020-11-04 23:30:10 +0300

    HW2

---
+++ homework2/task2/major_find.py
@@ -0,0 +1,39 @@
+"""
+Given an array of size n, find the most common and the least common elements.
+The most common element is the element that appears more than n // 2 times.
+The least common element is the element that appears fewer than other.
+
+You may assume that the array is non-empty and the most common element
+always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3, 2
+
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2, 1
+
+"""
+from typing import List, Tuple
+
+
+def major_and_minor_elem(inp: List) -> Tuple[int, int]:
+    dict = {}
+    for i in inp:
+        if i in dict:
+            dict[i] += 1
+        else:
+            dict[i] = 1
+    minor_value, major_value = dict[inp[0]], dict[inp[0]]
+    minor, major = inp[0], inp[0]
+    for key in dict:
+        if minor_value > dict[key]:
+            minor_value = dict[key]
+            minor = key
+        elif major_value < dict[key]:
+            major_value = dict[key]
+            major = key
+    return (major, minor)
