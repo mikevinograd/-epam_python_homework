ce56f6e33e609aa3f505022cc5d72e5c3e228e30
mikevinograd <33091915+mikevinograd@users.noreply.github.com>
2020-11-08 21:04:11 +0300

    hw2

--- homework2/task5/hw5.py
+++ homework2/task5/hw5.py
@@ -14,3 +14,19 @@ assert = custom_range(string.ascii_lowercase, 'g', 'p') == ['g', 'h', 'i', 'j', 
 assert = custom_range(string.ascii_lowercase, 'p', 'g', -2) == ['p', 'n', 'l', 'j', 'h']
 
 """
+import string
+
+def custom_range(iterable_values, stop:str, start=None, step=1):
+    # return iterable_values[start:stop:step]
+    val_list = list(iterable_values)
+    if start is None:
+        return iterable_values[0:val_list.index(stop):step]
+    else:
+        start, stop = stop, start
+        return iterable_values[val_list.index(start):val_list.index(stop):step]
+
+print(custom_range(string.ascii_lowercase, 'g'))
+print(custom_range(string.ascii_lowercase, "g"))
+print(custom_range(string.ascii_lowercase, "g", "p"))
+print(custom_range(string.ascii_lowercase, "p", "g", -2))
+print(custom_range("pure", "r"))
