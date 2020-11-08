08720dfc83b0716d8f7cff4b293da7a41192c394
mikevinograd <33091915+mikevinograd@users.noreply.github.com>
2020-11-08 22:11:31 +0300

    hw2

--- homework2/task4/cache.py
+++ homework2/task4/cache.py
@@ -18,21 +18,40 @@ val_2 = cache_func(*some)
 assert val_1 is val_2
 
 """
-from collections import Callable
+from typing  import Any, Sequence, Callable
+import functools
 
-
-def my_func(val, cache=dict()):
-    if val not in cache:
-        # compute val
-        res = 2 * val
-        print(f'{val} compute')
-        cache[val] = res
-    return cache[val]
+def my_func(val):
+    return val
 
 my_func(100)
 my_func(200)
 my_func(100)
 
 
-# def cache(func: Callable) -> Callable:
-#     ...
+def cache(func: Callable) -> Callable: #memoized
+    # use memoization technique
+    memoization_func = func
+
+    def wrapper(*args):
+        return memoization_func(args)
+
+    return memoization_func
+
+    # cache_dict = {}
+    #
+    # # @functools.wraps(func)
+    # def inner(*args):
+    #     # key = args, tuple(sorted(kwargs.items()))
+    #     if args in cache_dict:
+    #         return cache_dict[args]
+    #     else:
+    #         bi = cache_dict[args] = func(*args, **kwargs)
+    #         return bi
+    # return inner
+
+a = cache(my_func(100))
+b = cache(my_func(200))
+c = cache(my_func(100))
+print(id(a), id(c))
+print(a is b)
