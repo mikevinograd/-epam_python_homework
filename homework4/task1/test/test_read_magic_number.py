8df543457a7b99fe7b991fde8277efd378f6f13f
mikevinograd <33091915+mikevinograd@users.noreply.github.com>
2020-11-18 00:10:15 +0300

    hw4

--- homework4/task1/test/test_read_magic_number.py
+++ homework4/task1/test/test_read_magic_number.py
@@ -15,20 +15,46 @@ def file_gen(file_pass, val):
 
 
 @pytest.mark.parametrize(
-    ["file_pass", "val", "ground_truth"],
+    ["file_pass", "val"],
     [
-        ('data0.txt', '1', True),
-        ('data1.txt', '2.5', True),
-        ('data2.txt', '2.99\n56\n24\n', True),
-        ('data3.txt', '1\n', True),
+        ('data0.txt', '1'),
+        ('data1.txt', '2.5'),
+        ('data2.txt', '2.99\n56\n24\n'),
+        ('data3.txt', '1\n'),
     ],
 )
-def test_read_magic_number(file_pass, val, ground_truth, file_gen):
+def test_read_magic_number_true(file_pass, val, file_gen):
     # print(file_pass, val)
     actual_result = read_magic_number(file_pass)
     os.remove(file_pass)
-    assert actual_result is ground_truth
+    assert actual_result is True
 
-# open(os.path.join(os.pardir, 'task1\data1.txt'), "w")
-# test_read_magic_number('data1.txt', 2.5)
-# open( '../testfile.txt', "w")
+@pytest.mark.parametrize(
+    ["file_pass", "val"],
+    [
+        ('data0.txt', '-10'),
+        ('data1.txt', '0'),
+        ('data2.txt', '3\n56\n24\n'),
+        ('data3.txt', '0.99\n'),
+    ],
+)
+def test_read_magic_number_false(file_pass, val, file_gen):
+    # print(file_pass, val)
+    actual_result = read_magic_number(file_pass)
+    os.remove(file_pass)
+    assert actual_result is False
+
+@pytest.mark.parametrize(
+    ["file_pass", "val"],
+    [
+        ('data0.txt', 'sad'),
+        ('data1.txt', 'qwd'),
+        ('data2.txt', ''),
+        ('data3.txt', '1error'),
+    ],
+)
+def test_read_magic_number_value_error(file_pass, val, file_gen):
+    with pytest.raises(ValueError):
+        read_magic_number(file_pass)
+    os.remove(file_pass)
+
