0d5f7e1faaa53b643a136bc58313f54da32fa977
mikevinograd <33091915+mikevinograd@users.noreply.github.com>
2020-12-23 00:23:47 +0300

    hw11

--- homework11/task1/dublication_var.py
+++ homework11/task1/dublication_var.py
@@ -22,3 +22,30 @@ assert ColorsEnum.RED == "RED"
 assert SizesEnum.XL == "XL"
 """
 
+class SimplifiedEnum(type):
+
+    def __getattr__(self, key):
+        # for x, y in vars(self).items():
+        #
+        #     if not x.endswith("__"):
+        #         return x, y
+
+        keys = [attr for attr in vars(self) if not callable(getattr(self, attr)) and not attr.endswith("__")]
+        return self.__dict__.get()
+
+class ColorsEnum(metaclass=SimplifiedEnum):
+    __keys = ("RED", "BLUE", "ORANGE", "BLACK")
+
+print(ColorsEnum.RED)
+# print(ColorsEnum.__dir__(ColorsEnum))
+#
+# class Example(object):
+#     bool143 = True
+#     bool2 = True
+#     blah = False
+#     foo = True
+#     foobar2000 = False
+#
+# example = Example()
+# members = [attr for attr in dir(example) if not callable(getattr(example, attr)) and not attr.endswith("__")]
+# print (members)
---
+++ homework11/task2/discount.py
@@ -0,0 +1,43 @@
+"""
+You are given the following code:
+class Order:
+    morning_discount = 0.25
+    def __init__(self, price):
+        self.price = price
+    def final_price(self):
+        return self.price - self.price * self.morning_discount
+Make it possible to use different discount programs.
+Hint: use strategy behavioural OOP pattern.
+https://refactoring.guru/design-patterns/strategy
+Example of the result call:
+def morning_discount(order):
+    ...
+def elder_discount(order):
+    ...
+order_1 = Order(100, morning_discount)
+assert order_1.final_price() == 50
+order_2 = Order(100, elder_discount)
+assert order_1.final_price() == 10
+"""
+
+
+class Order:
+    morning_discount = 0.25
+
+    def __init__(self, price, discount):
+        self.price = price
+        self.discount = discount
+
+    def final_price(self):
+        return self.price - self.discount(self.price)
+
+
+def morning_discount(order):
+    return order * 0.5
+
+
+def elder_discount(order):
+    return order * 0.9
+
+order_1 = Order(100, elder_discount)
+print(order_1.final_price())
